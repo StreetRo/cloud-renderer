71891623ac4f2f3c4e9b8bde08ad69567f598da4
z <zach@zyoung.dev>
2021-04-16 12:20:13 -0700

    Working Test Application
    works on Linux
    main work will be done in src/clouds.{h,cpp}
    keeps majority of project code skeleton to aid cross compilation

---
+++ src/clouds.cpp
@@ -0,0 +1,544 @@
+#include <cmath>
+#include <glad/glad.h>
+
+#include <CGL/vector3D.h>
+#include <nanogui/nanogui.h>
+
+#include "clouds.h"
+
+#include "camera.h"
+#include "misc/camera_info.h"
+#include "misc/file_utils.h"
+// Needed to generate stb_image binaries. Should only define in exactly one source file importing stb_image.h.
+#define STB_IMAGE_IMPLEMENTATION
+#include "misc/stb_image.h"
+
+using namespace nanogui;
+using namespace std;
+
+Vector3D load_texture(int frame_idx, GLuint handle, const char* where) {
+  Vector3D size_retval;
+  
+  if (strlen(where) == 0) return size_retval;
+  
+  glActiveTexture(GL_TEXTURE0 + frame_idx);
+  glBindTexture(GL_TEXTURE_2D, handle);
+  
+  
+  int img_x, img_y, img_n;
+  unsigned char* img_data = stbi_load(where, &img_x, &img_y, &img_n, 3);
+  size_retval.x = img_x;
+  size_retval.y = img_y;
+  size_retval.z = img_n;
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img_x, img_y, 0, GL_RGB, GL_UNSIGNED_BYTE, img_data);
+  stbi_image_free(img_data);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  
+  return size_retval;
+}
+
+void load_cubemap(int frame_idx, GLuint handle, const std::vector<std::string>& file_locs) {
+  glActiveTexture(GL_TEXTURE0 + frame_idx);
+  glBindTexture(GL_TEXTURE_CUBE_MAP, handle);
+  for (int side_idx = 0; side_idx < 6; ++side_idx) {
+    int img_x, img_y, img_n;
+    unsigned char* img_data = stbi_load(file_locs[side_idx].c_str(), &img_x, &img_y, &img_n, 3);
+    glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + side_idx, 0, GL_RGB, img_x, img_y, 0, GL_RGB, GL_UNSIGNED_BYTE, img_data);
+    stbi_image_free(img_data);
+    std::cout << "Side " << side_idx << " has dimensions " << img_x << ", " << img_y << std::endl;
+
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+  }
+}
+
+void Clouds::load_textures() {
+  glGenTextures(1, &m_gl_texture_1);
+  glGenTextures(1, &m_gl_texture_2);
+  glGenTextures(1, &m_gl_texture_3);
+  glGenTextures(1, &m_gl_texture_4);
+  glGenTextures(1, &m_gl_cubemap_tex);
+  
+  m_gl_texture_1_size = load_texture(1, m_gl_texture_1, (m_project_root + "/textures/texture_1.png").c_str());
+  m_gl_texture_2_size = load_texture(2, m_gl_texture_2, (m_project_root + "/textures/texture_2.png").c_str());
+  m_gl_texture_3_size = load_texture(3, m_gl_texture_3, (m_project_root + "/textures/texture_3.png").c_str());
+  m_gl_texture_4_size = load_texture(4, m_gl_texture_4, (m_project_root + "/textures/texture_4.png").c_str());
+  
+  std::cout << "Texture 1 loaded with size: " << m_gl_texture_1_size << std::endl;
+  std::cout << "Texture 2 loaded with size: " << m_gl_texture_2_size << std::endl;
+  std::cout << "Texture 3 loaded with size: " << m_gl_texture_3_size << std::endl;
+  std::cout << "Texture 4 loaded with size: " << m_gl_texture_4_size << std::endl;
+  
+  std::vector<std::string> cubemap_fnames = {
+    m_project_root + "/textures/cube/posx.jpg",
+    m_project_root + "/textures/cube/negx.jpg",
+    m_project_root + "/textures/cube/posy.jpg",
+    m_project_root + "/textures/cube/negy.jpg",
+    m_project_root + "/textures/cube/posz.jpg",
+    m_project_root + "/textures/cube/negz.jpg"
+  };
+  
+  load_cubemap(5, m_gl_cubemap_tex, cubemap_fnames);
+  std::cout << "Loaded cubemap texture" << std::endl;
+}
+
+void Clouds::load_shaders() {
+  std::set<std::string> shader_folder_contents;
+  bool success = FileUtils::list_files_in_directory(m_project_root + "/shaders", shader_folder_contents);
+  if (!success) {
+    std::cout << "Error: Could not find the shaders folder!" << std::endl;
+  }
+  
+  std::string std_vert_shader = m_project_root + "/shaders/Default.vert";
+  
+  for (const std::string& shader_fname : shader_folder_contents) {
+    std::string file_extension;
+    std::string shader_name;
+    
+    FileUtils::split_filename(shader_fname, shader_name, file_extension);
+    
+    if (file_extension != "frag") {
+      std::cout << "Skipping non-shader file: " << shader_fname << std::endl;
+      continue;
+    }
+    
+    std::cout << "Found shader file: " << shader_fname << std::endl;
+    
+    // Check if there is a proper .vert shader or not for it
+    std::string vert_shader = std_vert_shader;
+    std::string associated_vert_shader_path = m_project_root + "/shaders/" + shader_name + ".vert";
+    if (FileUtils::file_exists(associated_vert_shader_path)) {
+      vert_shader = associated_vert_shader_path;
+    }
+    
+    std::shared_ptr<GLShader> nanogui_shader = make_shared<GLShader>();
+    nanogui_shader->initFromFiles(shader_name, vert_shader,
+                                  m_project_root + "/shaders/" + shader_fname);
+    
+    // Special filenames are treated a bit differently
+    ShaderTypeHint hint;
+    if (shader_name == "Wireframe") {
+      hint = ShaderTypeHint::WIREFRAME;
+      std::cout << "Type: Wireframe" << std::endl;
+    }
+    
+    UserShader user_shader(shader_name, nanogui_shader, hint);
+    
+    shaders.push_back(user_shader);
+    shaders_combobox_names.push_back(shader_name);
+  }
+  
+  // Assuming that it's there, use "Wireframe" by default
+  for (size_t i = 0; i < shaders_combobox_names.size(); ++i) {
+    if (shaders_combobox_names[i] == "Wireframe") {
+      active_shader_idx = i;
+      break;
+    }
+  }
+}
+
+Clouds::Clouds(std::string project_root, Screen *screen)
+: m_project_root(project_root) {
+  this->screen = screen;
+  
+  this->load_shaders();
+  this->load_textures();
+
+  glEnable(GL_PROGRAM_POINT_SIZE);
+  glEnable(GL_DEPTH_TEST);
+}
+
+Clouds::~Clouds() {
+  for (auto shader : shaders) {
+    shader.nanogui_shader->free();
+  }
+
+  glDeleteTextures(1, &m_gl_texture_1);
+  glDeleteTextures(1, &m_gl_texture_2);
+  glDeleteTextures(1, &m_gl_texture_3);
+  glDeleteTextures(1, &m_gl_texture_4);
+  glDeleteTextures(1, &m_gl_cubemap_tex);
+}
+
+/**
+ * Initializes the clouds simulation and spawns a new thread to separate
+ * rendering from simulation.
+ */
+void Clouds::init() {
+  // Initialize GUI
+  screen->setSize(default_window_size);
+  initGUI(screen);
+
+  // Initialize camera
+
+  CGL::Collada::CameraInfo camera_info;
+  camera_info.hFov = 50;
+  camera_info.vFov = 35;
+  camera_info.nClip = 0.01;
+  camera_info.fClip = 10000;
+
+  // Try to intelligently figure out the camera target
+
+  Vector3D avg_pm_position(0, 0, 0);
+
+  CGL::Vector3D target(avg_pm_position.x, avg_pm_position.y / 2,
+                       avg_pm_position.z);
+  CGL::Vector3D c_dir(0., 0., 0.);
+  canonical_view_distance = 2;
+  scroll_rate = canonical_view_distance / 10;
+
+  view_distance = canonical_view_distance * 2;
+  min_view_distance = canonical_view_distance / 10.0;
+  max_view_distance = canonical_view_distance * 20.0;
+
+  // canonicalCamera is a copy used for view resets
+
+  camera.place(target, acos(c_dir.y), atan2(c_dir.x, c_dir.z), view_distance,
+               min_view_distance, max_view_distance);
+  canonicalCamera.place(target, acos(c_dir.y), atan2(c_dir.x, c_dir.z),
+                        view_distance, min_view_distance, max_view_distance);
+
+  screen_w = default_window_size(0);
+  screen_h = default_window_size(1);
+
+  camera.configure(camera_info, screen_w, screen_h);
+  canonicalCamera.configure(camera_info, screen_w, screen_h);
+}
+
+bool Clouds::isAlive() { return is_alive; }
+
+void Clouds::drawContents() {
+  glEnable(GL_DEPTH_TEST);
+
+  // Bind the active shader
+  const UserShader& active_shader = shaders[active_shader_idx];
+
+  GLShader &shader = *active_shader.nanogui_shader;
+  shader.bind();
+
+  // Prepare the camera projection matrix
+  Matrix4f model;
+  model.setIdentity();
+
+  Matrix4f view = getViewMatrix();
+  Matrix4f projection = getProjectionMatrix();
+
+  Matrix4f viewProjection = projection * view;
+
+  shader.setUniform("u_model", model);
+  shader.setUniform("u_view_projection", viewProjection);
+
+
+
+  switch (active_shader.type_hint) {
+  case WIREFRAME:
+ 
+    Vector3D cam_pos = camera.position();
+    shader.setUniform("u_cam_pos", Vector3f(cam_pos.x, cam_pos.y, cam_pos.z), false);
+    shader.setUniform("u_light_pos", Vector3f(0.5, 2, 2), false);
+    shader.setUniform("u_light_intensity", Vector3f(3, 3, 3), false);
+
+    drawWireframe(shader);
+    break;
+  }
+}
+
+void Clouds::drawWireframe(GLShader &shader) {
+  MatrixXf positions(3, 3);
+
+  // RED :: +y +x +z
+  positions <<  1.0,  0.0, 0.0,
+                0.0,  1.0, 0.0,
+                0.0,  0.0, 1.0;
+
+  shader.uploadAttrib( "in_position", positions, false );
+  shader.setUniform("u_color", nanogui::Color(1.0f, 0.0f, 0.0f, 1.0f), false);
+  shader.drawArray( GL_TRIANGLES, 0, 3 );
+
+  // BLUE :: +y -x -z
+  positions <<  -1.0,  0.0, 0.0,
+                0.0,  1.0, 0.0,
+                0.0,  0.0, -1.0;
+
+  shader.uploadAttrib( "in_position", positions, false );
+  shader.setUniform("u_color", nanogui::Color(0.0f, 0.0f, 1.0f, 1.0f), false);
+  shader.drawArray( GL_TRIANGLES, 0, 3 );
+
+  // GREEN :: +y +x -z
+  positions <<  1.0,  0.0, 0.0,
+                0.0,  1.0, 0.0,
+                0.0,  0.0, -1.0;
+
+  shader.uploadAttrib( "in_position", positions, false );
+  shader.setUniform("u_color", nanogui::Color(0.0f, 1.0f, 0.0f, 1.0f), false);
+  shader.drawArray( GL_TRIANGLES, 0, 3 );
+
+  // PURPLE :: +y -x +z
+  positions <<  -1.0,  0.0, 0.0,
+                0.0,  1.0, 0.0,
+                0.0,  0.0, 1.0;
+
+  shader.uploadAttrib( "in_position", positions, false );
+  shader.setUniform("u_color", nanogui::Color(1.0f, 0.0f, 1.0f, 1.0f), false);
+  shader.drawArray( GL_TRIANGLES, 0, 3 );
+
+  // BOTTOM
+  positions <<  0.0,  1.0, 0.0,
+                0.0,  0.0, 0.0,
+               -1.0,  0.0, 1.0;
+
+  shader.uploadAttrib( "in_position", positions, false );
+  shader.setUniform("u_color", nanogui::Color(1.0f, 1.0f, 1.0f, 1.0f), false);
+  shader.drawArray( GL_TRIANGLES, 0, 3 );
+
+  positions <<  0.0, -1.0, 0.0,
+                0.0,  0.0, 0.0,
+               -1.0,  0.0, 1.0;
+
+  shader.uploadAttrib( "in_position", positions, false );
+  shader.setUniform("u_color", nanogui::Color(1.0f, 1.0f, 1.0f, 1.0f), false);
+  shader.drawArray( GL_TRIANGLES, 0, 3 );
+}
+
+// ----------------------------------------------------------------------------
+// CAMERA CALCULATIONS
+//
+// On initial load, +z faces camera, +x points right, +y points up
+// ----------------------------------------------------------------------------
+
+void Clouds::resetCamera() { camera.copy_placement(canonicalCamera); }
+
+Matrix4f Clouds::getProjectionMatrix() {
+  Matrix4f perspective;
+  perspective.setZero();
+
+  double cam_near = camera.near_clip();
+  double cam_far = camera.far_clip();
+
+  double theta = camera.v_fov() * PI / 360;
+  double range = cam_far - cam_near;
+  double invtan = 1. / tanf(theta);
+
+  perspective(0, 0) = invtan / camera.aspect_ratio();
+  perspective(1, 1) = invtan;
+  perspective(2, 2) = -(cam_near + cam_far) / range;
+  perspective(3, 2) = -1;
+  perspective(2, 3) = -2 * cam_near * cam_far / range;
+  perspective(3, 3) = 0;
+
+  return perspective;
+}
+
+Matrix4f Clouds::getViewMatrix() {
+  Matrix4f lookAt;
+  Matrix3f R;
+
+  lookAt.setZero();
+
+  // Convert CGL vectors to Eigen vectors
+  // TODO: Find a better way to do this!
+
+  CGL::Vector3D c_pos = camera.position();
+  CGL::Vector3D c_udir = camera.up_dir();
+  CGL::Vector3D c_target = camera.view_point();
+
+  Vector3f eye(c_pos.x, c_pos.y, c_pos.z);
+  Vector3f up(c_udir.x, c_udir.y, c_udir.z);
+  Vector3f target(c_target.x, c_target.y, c_target.z);
+
+  R.col(2) = (eye - target).normalized();
+  R.col(0) = up.cross(R.col(2)).normalized();
+  R.col(1) = R.col(2).cross(R.col(0));
+
+  lookAt.topLeftCorner<3, 3>() = R.transpose();
+  lookAt.topRightCorner<3, 1>() = -R.transpose() * eye;
+  lookAt(3, 3) = 1.0f;
+
+  return lookAt;
+}
+
+// ----------------------------------------------------------------------------
+// EVENT HANDLING
+// ----------------------------------------------------------------------------
+
+bool Clouds::cursorPosCallbackEvent(double x, double y) {
+  if (left_down && !middle_down && !right_down) {
+    if (ctrl_down) {
+      mouseRightDragged(x, y);
+    } else {
+      mouseLeftDragged(x, y);
+    }
+  } else if (!left_down && !middle_down && right_down) {
+    mouseRightDragged(x, y);
+  } else if (!left_down && !middle_down && !right_down) {
+    mouseMoved(x, y);
+  }
+
+  mouse_x = x;
+  mouse_y = y;
+
+  return true;
+}
+
+bool Clouds::mouseButtonCallbackEvent(int button, int action, int modifiers) {
+  switch (action) {
+  case GLFW_PRESS:
+    switch (button) {
+    case GLFW_MOUSE_BUTTON_LEFT:
+      left_down = true;
+      break;
+    case GLFW_MOUSE_BUTTON_MIDDLE:
+      middle_down = true;
+      break;
+    case GLFW_MOUSE_BUTTON_RIGHT:
+      right_down = true;
+      break;
+    }
+    return true;
+
+  case GLFW_RELEASE:
+    switch (button) {
+    case GLFW_MOUSE_BUTTON_LEFT:
+      left_down = false;
+      break;
+    case GLFW_MOUSE_BUTTON_MIDDLE:
+      middle_down = false;
+      break;
+    case GLFW_MOUSE_BUTTON_RIGHT:
+      right_down = false;
+      break;
+    }
+    return true;
+  }
+
+  return false;
+}
+
+void Clouds::mouseMoved(double x, double y) { y = screen_h - y; }
+
+void Clouds::mouseLeftDragged(double x, double y) {
+  float dx = x - mouse_x;
+  float dy = y - mouse_y;
+
+  camera.rotate_by(-dy * (PI / screen_h), -dx * (PI / screen_w));
+}
+
+void Clouds::mouseRightDragged(double x, double y) {
+  camera.move_by(mouse_x - x, y - mouse_y, canonical_view_distance);
+}
+
+bool Clouds::keyCallbackEvent(int key, int scancode, int action,
+                                      int mods) {
+  ctrl_down = (bool)(mods & GLFW_MOD_CONTROL);
+
+  if (action == GLFW_PRESS) {
+    switch (key) {
+    case GLFW_KEY_ESCAPE:
+      is_alive = false;
+      break;
+    case ' ':
+      resetCamera();
+      break;
+    case 'p':
+    case 'P':
+      is_paused = !is_paused;
+      break;
+    case 'n':
+    case 'N':
+      if (is_paused) {
+        is_paused = false;
+        drawContents();
+        is_paused = true;
+      }
+      break;
+    }
+  }
+
+  return true;
+}
+
+bool Clouds::dropCallbackEvent(int count, const char **filenames) {
+  return true;
+}
+
+bool Clouds::scrollCallbackEvent(double x, double y) {
+  camera.move_forward(y * scroll_rate);
+  return true;
+}
+
+bool Clouds::resizeCallbackEvent(int width, int height) {
+  screen_w = width;
+  screen_h = height;
+
+  camera.set_screen_size(screen_w, screen_h);
+  return true;
+}
+
+void Clouds::initGUI(Screen *screen) {
+  Window *window;
+  
+  window = new Window(screen, "Cloud Simulation");
+  window->setPosition(Vector2i( 15, 15 ));
+  window->setLayout(new GroupLayout(15, 6, 14, 5));
+
+  new Label(window, "Buttons", "sans-bold");
+  {
+    Button *b = new Button(window, "Button 1");
+    b->setFlags(Button::ToggleButton);
+    b->setPushed( button1 );
+    b->setFontSize(14);
+    b->setChangeCallback( [this](bool state) {  } );
+  }
+
+  new Label(window, "Parameters", "sans-bold");
+  {
+    Widget *panel = new Widget(window);
+    GridLayout *layout =
+        new GridLayout(Orientation::Horizontal, 2, Alignment::Middle, 5, 5);
+    layout->setColAlignment({Alignment::Maximum, Alignment::Fill});
+    layout->setSpacing(0, 10);
+    panel->setLayout(layout);
+
+    new Label(panel, "param 1 :", "sans-bold");
+
+    FloatBox<double> *fb = new FloatBox<double>(panel);
+    fb->setEditable(true);
+    fb->setFixedSize(Vector2i(100, 20));
+    fb->setFontSize(14);
+    fb->setValue( param1 );
+    fb->setUnits("units");
+    fb->setSpinnable(true);
+    fb->setCallback([this](float value) {  });
+  }
+
+  new Label(window, "Sliders", "sans-bold");
+  {
+    Widget *panel = new Widget(window);
+    panel->setLayout(
+        new BoxLayout(Orientation::Horizontal, Alignment::Middle, 0, 5));
+
+    Slider *s = new Slider(panel);
+    s->setValue( slider );
+    s->setFixedWidth(105);
+
+    TextBox *percentage = new TextBox(panel);
+    percentage->setFixedWidth(75);
+    percentage->setValue(to_string( slider ));
+    percentage->setUnits("%");
+    percentage->setFontSize(14);
+
+    s->setCallback([percentage](float value) {
+      percentage->setValue(std::to_string(value));
+    });
+    s->setFinalCallback([&](float value) {
+      slider = value;
+    });
+  }
+}
---
+++ src/main.cpp
@@ -0,0 +1,230 @@
+#include <iostream>
+#include <fstream>
+#include <nanogui/nanogui.h>
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef _WIN32
+#include "misc/getopt.h" // getopt for windows
+#else
+#include <getopt.h>
+#include <unistd.h>
+#endif
+#include <unordered_set>
+#include <stdlib.h> // atoi for getopt inputs
+
+#include "CGL/CGL.h"
+#include "clouds.h"
+#include "json.hpp"
+#include "misc/file_utils.h"
+
+typedef uint32_t gid_t;
+
+using namespace std;
+using namespace nanogui;
+
+using json = nlohmann::json;
+
+Clouds*     app    = nullptr;
+GLFWwindow* window = nullptr;
+Screen*     screen = nullptr;
+
+void error_callback(int error, const char* description) {
+  puts(description);
+}
+
+void createGLContexts() {
+  if (!glfwInit()) {
+    return;
+  }
+
+  glfwSetTime(0);
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  glfwWindowHint(GLFW_SAMPLES, 0);
+  glfwWindowHint(GLFW_RED_BITS, 8);
+  glfwWindowHint(GLFW_GREEN_BITS, 8);
+  glfwWindowHint(GLFW_BLUE_BITS, 8);
+  glfwWindowHint(GLFW_ALPHA_BITS, 8);
+  glfwWindowHint(GLFW_STENCIL_BITS, 8);
+  glfwWindowHint(GLFW_DEPTH_BITS, 24);
+  glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+  // Create a GLFWwindow object
+  window = glfwCreateWindow(800, 800, "Cloud Simulator", nullptr, nullptr);
+  if (window == nullptr) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return;
+  }
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    throw std::runtime_error("Could not initialize GLAD!");
+  }
+  glGetError(); // pull and ignore unhandled errors like GL_INVALID_ENUM
+
+  glClearColor(0.2f, 0.25f, 0.3f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  // Create a nanogui screen and pass the glfw pointer to initialize
+  screen = new Screen();
+  screen->initialize(window, true);
+
+  int width, height;
+  glfwGetFramebufferSize(window, &width, &height);
+  glViewport(0, 0, width, height);
+  glfwSwapInterval(1);
+  glfwSwapBuffers(window);
+}
+
+void setGLFWCallbacks() {
+  glfwSetCursorPosCallback(window, [](GLFWwindow *, double x, double y) {
+    if (!screen->cursorPosCallbackEvent(x, y)) {
+      app->cursorPosCallbackEvent(x / screen->pixelRatio(),
+                                  y / screen->pixelRatio());
+    }
+  });
+
+  glfwSetMouseButtonCallback(
+      window, [](GLFWwindow *, int button, int action, int modifiers) {
+        if (!screen->mouseButtonCallbackEvent(button, action, modifiers) ||
+            action == GLFW_RELEASE) {
+          app->mouseButtonCallbackEvent(button, action, modifiers);
+        }
+      });
+
+  glfwSetKeyCallback(
+      window, [](GLFWwindow *, int key, int scancode, int action, int mods) {
+        if (!screen->keyCallbackEvent(key, scancode, action, mods)) {
+          app->keyCallbackEvent(key, scancode, action, mods);
+        }
+      });
+
+  glfwSetCharCallback(window, [](GLFWwindow *, unsigned int codepoint) {
+    screen->charCallbackEvent(codepoint);
+  });
+
+  glfwSetDropCallback(window,
+                      [](GLFWwindow *, int count, const char **filenames) {
+                        screen->dropCallbackEvent(count, filenames);
+                        app->dropCallbackEvent(count, filenames);
+                      });
+
+  glfwSetScrollCallback(window, [](GLFWwindow *, double x, double y) {
+    if (!screen->scrollCallbackEvent(x, y)) {
+      app->scrollCallbackEvent(x, y);
+    }
+  });
+
+  glfwSetFramebufferSizeCallback(window,
+                                 [](GLFWwindow *, int width, int height) {
+                                   screen->resizeCallbackEvent(width, height);
+                                   app->resizeCallbackEvent(width, height);
+                                 });
+}
+
+void usageError(const char *binaryName) {
+  printf("Usage: %s [options]\n\n", binaryName);
+  printf("\n");
+  exit(-1);
+}
+
+void incompleteObjectError(const char *object, const char *attribute) {
+  cout << "Incomplete " << object << " definition, missing " << attribute << endl;
+  exit(-1);
+}
+
+bool is_valid_project_root(const std::string& search_path) {
+    std::stringstream ss;
+    ss << search_path;
+    ss << "/";
+    ss << "shaders/Default.vert";
+    
+    return FileUtils::file_exists(ss.str());
+}
+
+// Attempt to locate the project root automatically
+bool find_project_root(const std::vector<std::string>& search_paths, std::string& retval) {
+  for (std::string search_path : search_paths) {
+    if (is_valid_project_root(search_path)) {
+      retval = search_path;
+      return true;
+    }
+  }
+  return false;
+}
+
+int main( int argc, char **argv ) {
+  std::vector<std::string> search_paths = {
+    ".",
+    "..",
+    "../..",
+    "../../.."
+  };
+  std::string project_root;
+  bool found_project_root = find_project_root(search_paths, project_root);
+  
+  std::string file_to_load_from;
+  bool file_specified = false;
+
+  int opt;
+  while ( (opt = getopt (argc, argv, "a:")) != -1 ) {
+    switch ( opt ) {
+      case 'a': {
+        std::cout << "got option -a\n";
+        break;
+      }
+      default: {
+        usageError(argv[0]);
+        break;
+      }
+    }
+  }
+  
+  if (!found_project_root) {
+    std::cout << "Error: Could not find required file \"shaders/Default.vert\" anywhere!" << std::endl;
+    return -1;
+  } else {
+    std::cout << "Loading files starting from: " << project_root << std::endl;
+  }
+
+  glfwSetErrorCallback(error_callback);
+
+  createGLContexts();
+
+  // Initialize the CloudSimulator object
+  app = new Clouds(project_root, screen);
+  app->init();
+
+  // Call this after all the widgets have been defined
+  screen->setVisible(true);
+  screen->performLayout();
+
+  // Attach callbacks to the GLFW window
+  setGLFWCallbacks();
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+
+    glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    app->drawContents();
+
+    // Draw nanogui
+    screen->drawContents();
+    screen->drawWidgets();
+
+    glfwSwapBuffers(window);
+
+    if (!app->isAlive()) {
+      glfwSetWindowShouldClose(window, 1);
+    }
+  }
+
+  return 0;
+}
