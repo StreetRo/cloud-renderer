f8662f83adefff833183819cb8e244d20267f013
z <zach@zyoung.dev>
2021-04-26 12:33:17 -0700

    Set initial num_cells to 2

--- src/clouds.h
+++ src/clouds.h
@@ -135,7 +135,7 @@ private:
 
   // Local Persistent Objects
   int n_pts = 5000000;
-  int num_cells = 1;
+  int num_cells = 2;
   float pt_size = 10;
   MatrixXf* positions = nullptr;
   MatrixXf* density_pts = nullptr;
